79af7c1bd6a83bd2a22d00166bf58415c7032310
mielnikmagdalena <mielnikmagdalena@gmail.com>
2023-10-23 22:57:14 +0200

    first draft of hw2

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,84 @@
-const express = require('express')
+import express from "express";
+import Joi from "joi";
+import {
+  listContacts,
+  getContactById,
+  addContact,
+  removeContact,
+  updateContact,
+} from "../../models/contacts.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/", (req, res, next) => {
+  const contacts = listContacts();
+  res.json(contacts);
+});
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.get("/:contactId", (req, res, next) => {
+  const contactId = req.params.contactId;
+  const contact = getContactById(contactId);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  if (contact) {
+    res.json(contact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+});
 
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router.post("/", (req, res, next) => {
+  const { name, email, phone } = req.body;
 
-router.put('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+  // Walidacja danych wejściowych
+  const schema = Joi.object({
+    name: Joi.string().required(),
+    email: Joi.string().email().required(),
+    phone: Joi.string().required(),
+  });
 
-module.exports = router
+  const { error } = schema.validate({ name, email, phone });
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const newContact = addContact({ name, email, phone });
+  res.status(201).json(newContact);
+});
+
+router.delete("/:contactId", async (req, res, next) => {
+  const contactId = req.params.contactId;
+  const isRemoved = removeContact(contactId);
+
+  if (isRemoved) {
+    res.json({ message: "contact deleted" });
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+});
+
+router.put("/:contactId", (req, res, next) => {
+  const contactId = req.params.contactId;
+  const { name, email, phone } = req.body;
+
+  // Walidacja danych wejściowych
+  const schema = Joi.object({
+    name: Joi.string(),
+    email: Joi.string().email(),
+    phone: Joi.string(),
+  });
+
+  const { error } = schema.validate({ name, email, phone });
+  if (error) {
+    return res.status(400).json({ message: error.details[0].message });
+  }
+
+  const updatedContact = updateContact(contactId, { name, email, phone });
+
+  if (updatedContact) {
+    res.json(updatedContact);
+  } else {
+    res.status(404).json({ message: "Not found" });
+  }
+});
+
+export default router;
